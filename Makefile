e9acbfe8f7b2168904db4c7b1b30abc333a26543
Амир Абдрафиков <sequze@mail.ru>
2026-02-23 17:31:33 +0300

    Initial commit

---
+++ Makefile
@@ -0,0 +1,4 @@
+.PHONY:
+
+run_tests:
+
---
+++ tests/test_app.py
@@ -0,0 +1,7 @@
+from app import sum
+
+def test_sum():
+    assert sum(2, 3) == 5, "2 + 3 should equal 5"
+    assert sum(-1, 1) == 0, "-1 + 1 should equal 0"
+    print("All tests passed!")
+
